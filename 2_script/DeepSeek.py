7410f609e27815d11414a08fe9ecedb059ae5f12
xu_xiaolong <luckily126163@126.com>
2025-09-05 17:51:45 +0800

    代码提交

---
+++ 2_script/DeepSeek.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-5b39f97595444f6cb0339e3c55bb0e76", base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
